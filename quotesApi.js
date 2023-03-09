a5c54e9a2534a085aea26f88643427650bb160de
Liam Bowie <liambowie16@gmail.com>
2023-03-09 13:18:37 +0000

    Fine tune the GET /api/quotes route

--- quotesApi.js
+++ quotesApi.js
@@ -6,14 +6,11 @@ const { getRandomElement, getIndexById } = require('./utils');
 quoteRouter = express.Router();
 
 quoteRouter.get('/', (req, res, next) => {
-    let returnedQuotes = []; 
-    
-    if (req.query.person) {
-        returnedQuotes = quotes.filter(quote => quote.person === req.query.person);
+    if (req.query.person !== undefined) {
+        const returnedQuotes = quotes.filter(quote => quote.person === req.query.person);
         res.send({quotes: returnedQuotes});
     } else {
-        returnedQuotes = quotes;
-        res.send({quotes: returnedQuotes});
+        res.send({quotes: quotes});
     }
 })
 
