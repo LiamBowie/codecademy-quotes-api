6d479ab7c4ab930bd21d5db922da7ecac4ffd448
Liam Bowie <liambowie16@gmail.com>
2023-03-09 12:21:38 +0000

    Implement POST route to add a new quote to the quotes array if all of the correct information is sent by the user. Otherwise send a 400 error code.

--- quotesApi.js
+++ quotesApi.js
@@ -9,7 +9,6 @@ quoteRouter.get('/', (req, res, next) => {
     
     if (req.query.person) {
         returnedQuotes = quotes.filter(quote => quote.person === req.query.person);
-        
         res.send({quotes: returnedQuotes});
     } else {
         returnedQuotes = quotes;
@@ -20,6 +19,18 @@ quoteRouter.get('/', (req, res, next) => {
 quoteRouter.get('/random', (req, res, next) => {
     const quote = getRandomElement(quotes)
     res.send({quote: quote});
+})
+
+quoteRouter.post('/', (req, res, next) => {
+    const { quote, person } = req.query;
+    if (quote && person) {
+        const newQuote = {person: person, quote: quote};
+        quotes.push(newQuote);
+        res.status(201).send({quote: newQuote});
+    } else {
+        console.log('Error code 400');
+        res.status(400).send();
+    }
 })
 
 module.exports = quoteRouter;
