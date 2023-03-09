6f1ff4c215f2ef11d951369adcc9d66e160cdec0
Liam Bowie <liambowie16@gmail.com>
2023-03-09 12:04:12 +0000

    Implement GET routes on the Express server for /api/quotes and /api/quotes/random

--- quotesApi.js
+++ quotesApi.js
@@ -2,11 +2,24 @@ const express = require('express');
 const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
-
 quoteRouter = express.Router();
 
+quoteRouter.get('/', (req, res, next) => {
+    let returnedQuotes = []; 
+    
+    if (req.query.person) {
+        returnedQuotes = quotes.filter(quote => quote.person === req.query.person);
+        
+        res.send({quotes: returnedQuotes});
+    } else {
+        returnedQuotes = quotes;
+        res.send({quotes: returnedQuotes});
+    }
+})
+
 quoteRouter.get('/random', (req, res, next) => {
-    res.send(getRandomElement(quotes));
+    const quote = getRandomElement(quotes)
+    res.send({quote: quote});
 })
 
 module.exports = quoteRouter;
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 4001;
 app.use(express.static('public'));
 app.use('/api/quotes', quoteRouter);
 
-
 app.listen(PORT, () => {
     console.log('We are live!')
 })
